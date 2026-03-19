6c8cd35264aa76ebcbd5961a4818b9e7d4418694
Van <Van@DESKTOP-DHPETRI>
2026-03-19 23:04:31 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using 知识产权数字化平台.Views;
+
+namespace 知识产权数字化平台
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return new Shell();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //throw new NotImplementedException();
+        }   
+
+    }
+}
